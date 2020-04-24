7122ca69330bcf3657be840b2c7cad0307e263a1
David Langheiter <david@langheiter.com>
2020-04-24 02:54:21 +0200

    Added PrintingBolt

---
+++ storm/src/main/java/PrintingBolt.java
@@ -0,0 +1,17 @@
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.tuple.Tuple;
+
+public class PrintingBolt extends BaseBasicBolt {
+
+    @Override
+    public void execute(Tuple input, BasicOutputCollector collector) {
+        System.out.println(input);
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        
+    }
+}
